877f7ea1b6e7f56802f247a568e580fc157172e9
Ben Asher <benasher44@gmail.com>
2019-04-13 09:11:17 -0700

    windows fix

--- scripts/publishWin.bat
+++ scripts/publishWin.bat
@@ -1,7 +1,5 @@
 REM free some space
 
-POWERSHELL "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; (New-Object System.Net.WebClient).DownloadFile('https://github.com/ojdkbuild/ojdkbuild/releases/download/1.8.0.191-1/java-1.8.0-openjdk-1.8.0.191-1.b12.ojdkbuild.windows.x86_64.zip','java.zip')"
-POWERSHELL "Expand-Archive java.zip -DestinationPath c:\java8"
 DIR c:\java8
 DIR c:\java8\java-1.8.0-openjdk-1.8.0.191-1.b12.ojdkbuild.windows.x86_64
 SET JAVA_HOME=c:\java8\java-1.8.0-openjdk-1.8.0.191-1.b12.ojdkbuild.windows.x86_64
@@ -11,4 +9,4 @@ REM SET GRADLE_ERROR_LEVEL=%errorlevel%
 REM CALL gradlew.bat --stop
 REM exit /b %GRADLE_ERROR_LEVEL%
 
-CALL ..\gradlew.bat --no-daemon -s publishMingwPublicationToMavenRepository -PSONATYPE_USERNAME=$SONATYPE_USERNAME -PSONATYPE_PASSWORD=$SONATYPE_PASSWORD
+CALL gradlew.bat --no-daemon -s publishMingwPublicationToMavenRepository -PSONATYPE_USERNAME=$SONATYPE_USERNAME -PSONATYPE_PASSWORD=$SONATYPE_PASSWORD
