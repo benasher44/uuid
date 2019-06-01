59c2cb22cb4304bc6659c045c9aa91d598c87eca
Björn Schramke <bschramke@users.noreply.github.com>
2019-06-01 18:42:43 +0200

    Use SecureRandom on JVM (#16)
    - Use SecureRandom on JVM
    - Add getters for most and least significant bits to match Java UUID implementation
    - Improve JVM test coverage
    - Improve documentation syntax (KDoc)
    - Update .gitignore

---
+++ src/jvmTest/kotlin/com/benasher44/uuid/JunitUUIDTest.kt
@@ -0,0 +1,32 @@
+package com.benasher44.uuid
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class JunitUUIDTest {
+
+    @Test
+    fun shouldSetCorrectVersionBit() {
+        val uuidL = UUID()
+        val platformUuid = java.util.UUID(uuidL.mostSignificantBits, uuidL.leastSignificantBits)
+
+        assertEquals(platformUuid.version(), 4)
+    }
+
+    @Test
+    fun shouldMatchPlatformUUIDString() {
+        val uuidL = UUID()
+        val platformUuid = java.util.UUID(uuidL.mostSignificantBits, uuidL.leastSignificantBits)
+
+        assertEquals(uuidL.toString(), platformUuid.toString())
+    }
+
+    @Test
+    fun shouldMatchPlatformUUIDbytes() {
+        val uuidL = UUID()
+        val platformUuid = java.util.UUID.fromString(uuidL.toString())
+
+        assertEquals(uuidL.mostSignificantBits, platformUuid.mostSignificantBits)
+        assertEquals(uuidL.leastSignificantBits, platformUuid.leastSignificantBits)
+    }
+}
