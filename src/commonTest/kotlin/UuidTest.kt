cdfe9c7a605527591c39d5994611b60b3be0b316
Ben Asher <benasher44@gmail.com>
2019-12-29 15:15:05 -0500

    Fix parse tests (#68)
    - This fixes tests to use the non-deprecated function for parsing a UUID from a string

--- src/commonTest/kotlin/UuidTest.kt
+++ src/commonTest/kotlin/UuidTest.kt
@@ -27,7 +27,7 @@ class UuidTest {
     @Suppress("DEPRECATION")
     fun deprecated_parses_a_UUID_from_a_string() {
         val uuid = uuid4()
-        val uuidFromStr = Uuid.parse(uuid.toString())
+        val uuidFromStr = uuidFrom(uuid.toString())
         assertEquals(uuid, uuidFromStr)
         // double check hashcode equality, while we're here
         assertEquals(uuid.hashCode(), uuidFromStr.hashCode())
@@ -75,7 +75,7 @@ class UuidTest {
     @Test
     @Suppress("DEPRECATION")
     fun deprecated_parse_provides_higher_and_lower_bits() {
-        val uuid = Uuid.parse("c480d6ab-cb0c-427b-a9a6-19c5f8a146bd")!!
+        val uuid = uuidFrom("c480d6ab-cb0c-427b-a9a6-19c5f8a146bd")
         assertEquals(-6222257497095190851, uuid.leastSignificantBits)
         assertEquals(-4287190811922382213, uuid.mostSignificantBits)
     }
@@ -106,10 +106,10 @@ class UuidTest {
     @Test
     @Suppress("DEPRECATION")
     fun parse_variants_from_deprecated_parse() {
-        assertEquals(0, Uuid.parse("00000000-0000-0000-0000-000000000000")!!.variant, "Nil or NCS")
-        assertEquals(2, Uuid.parse("00000000-0000-0000-8000-000000000000")!!.variant, "RFC 4122")
-        assertEquals(6, Uuid.parse("00000000-0000-0000-c000-000000000000")!!.variant, "Microsoft")
-        assertEquals(7, Uuid.parse("00000000-0000-0000-e000-000000000000")!!.variant, "Future")
+        assertEquals(0, uuidFrom("00000000-0000-0000-0000-000000000000").variant, "Nil or NCS")
+        assertEquals(2, uuidFrom("00000000-0000-0000-8000-000000000000").variant, "RFC 4122")
+        assertEquals(6, uuidFrom("00000000-0000-0000-c000-000000000000").variant, "Microsoft")
+        assertEquals(7, uuidFrom("00000000-0000-0000-e000-000000000000").variant, "Future")
     }
 
     @Test
@@ -123,23 +123,23 @@ class UuidTest {
     @Test
     @Suppress("DEPRECATION")
     fun versions_from_deprecated_parse() {
-        assertEquals(0, Uuid.parse("00000000-0000-0000-0000-000000000000")!!.version, "Nil")
-        assertEquals(1, Uuid.parse("00000000-0000-1000-0000-000000000000")!!.version, "time-based")
-        assertEquals(2, Uuid.parse("00000000-0000-2000-0000-000000000000")!!.version, "DCE security")
-        assertEquals(3, Uuid.parse("00000000-0000-3000-0000-000000000000")!!.version, "name-based using MD5 hashing")
-        assertEquals(4, Uuid.parse("00000000-0000-4000-0000-000000000000")!!.version, "random or pseudo-random")
-        assertEquals(5, Uuid.parse("00000000-0000-5000-0000-000000000000")!!.version, "name-based using SHA-1 hashing")
+        assertEquals(0, uuidFrom("00000000-0000-0000-0000-000000000000").version, "Nil")
+        assertEquals(1, uuidFrom("00000000-0000-1000-0000-000000000000").version, "time-based")
+        assertEquals(2, uuidFrom("00000000-0000-2000-0000-000000000000").version, "DCE security")
+        assertEquals(3, uuidFrom("00000000-0000-3000-0000-000000000000").version, "name-based using MD5 hashing")
+        assertEquals(4, uuidFrom("00000000-0000-4000-0000-000000000000").version, "random or pseudo-random")
+        assertEquals(5, uuidFrom("00000000-0000-5000-0000-000000000000").version, "name-based using SHA-1 hashing")
 
-        assertEquals(6, Uuid.parse("00000000-0000-6000-0000-000000000000")!!.version, "future #1")
-        assertEquals(7, Uuid.parse("00000000-0000-7000-0000-000000000000")!!.version, "future #2")
-        assertEquals(8, Uuid.parse("00000000-0000-8000-0000-000000000000")!!.version, "future #3")
-        assertEquals(9, Uuid.parse("00000000-0000-9000-0000-000000000000")!!.version, "future #4")
-        assertEquals(10, Uuid.parse("00000000-0000-a000-0000-000000000000")!!.version, "future #5")
-        assertEquals(11, Uuid.parse("00000000-0000-b000-0000-000000000000")!!.version, "future #6")
-        assertEquals(12, Uuid.parse("00000000-0000-c000-0000-000000000000")!!.version, "future #7")
-        assertEquals(13, Uuid.parse("00000000-0000-d000-0000-000000000000")!!.version, "future #8")
-        assertEquals(14, Uuid.parse("00000000-0000-e000-0000-000000000000")!!.version, "future #9")
-        assertEquals(15, Uuid.parse("00000000-0000-f000-0000-000000000000")!!.version, "future #10")
+        assertEquals(6, uuidFrom("00000000-0000-6000-0000-000000000000").version, "future #1")
+        assertEquals(7, uuidFrom("00000000-0000-7000-0000-000000000000").version, "future #2")
+        assertEquals(8, uuidFrom("00000000-0000-8000-0000-000000000000").version, "future #3")
+        assertEquals(9, uuidFrom("00000000-0000-9000-0000-000000000000").version, "future #4")
+        assertEquals(10, uuidFrom("00000000-0000-a000-0000-000000000000").version, "future #5")
+        assertEquals(11, uuidFrom("00000000-0000-b000-0000-000000000000").version, "future #6")
+        assertEquals(12, uuidFrom("00000000-0000-c000-0000-000000000000").version, "future #7")
+        assertEquals(13, uuidFrom("00000000-0000-d000-0000-000000000000").version, "future #8")
+        assertEquals(14, uuidFrom("00000000-0000-e000-0000-000000000000").version, "future #9")
+        assertEquals(15, uuidFrom("00000000-0000-f000-0000-000000000000").version, "future #10")
     }
 
     @Test
