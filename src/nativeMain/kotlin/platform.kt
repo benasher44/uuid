7bbc398fb93453ef90bfb84ca60c7c40c36b438b
Ben Asher <ben.asher@autodesk.com>
2020-07-10 18:16:22 -0700

    put freeze back

--- src/nativeMain/kotlin/platform.kt
+++ src/nativeMain/kotlin/platform.kt
@@ -1,7 +1,10 @@
 package com.benasher44.uuid
 
+import kotlin.native.concurrent.freeze
 import platform.builtin.builtin_bswap16
 import platform.builtin.builtin_bswap32
+
+internal actual fun <T> T.freeze() = this.freeze()
 
 internal actual fun htonl(value: Int): Int {
     if (!Platform.isLittleEndian) return value
