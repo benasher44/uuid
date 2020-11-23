26fdfbeb0fd1f2c936b5acf12ac61ceb1bc2eddc
Ben Asher <benasher44@gmail.com>
2020-11-23 10:25:48 -0800

    Improve error handling on nix systems (#108)
    Improve error handling on native for open call — #107

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/), and this
 project adheres to [Semantic Versioning](https://semver.org/).
 
+## [0.2.3] - TBD
+### Changed
+- Improve error handling on nix systems (#108)
+
 ## [0.2.2] - 2020-08-27
 ### Added
 - Support for watchOS and tvOS (#98)
---
+++ src/nativeMain/kotlin/urandom.kt
@@ -0,0 +1,24 @@
+package com.benasher44.uuid
+
+import kotlinx.cinterop.ByteVar
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import platform.posix.O_RDONLY
+import platform.posix.close
+import platform.posix.errno
+import platform.posix.open
+
+internal fun bytesWithURandomFd(fdLambda: (Int, CPointer<ByteVar>) -> Unit): ByteArray {
+    return ByteArray(UUID_BYTES).also { bytes ->
+        val fd = open("/dev/urandom", O_RDONLY)
+        check(fd != -1) { "Failed to access /dev/urandom: $errno" }
+        try {
+            bytes.usePinned {
+                fdLambda(fd, it.addressOf(0))
+            }
+        } finally {
+            close(fd)
+        }
+    }
+}
