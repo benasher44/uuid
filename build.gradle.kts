84459c695adc7e3502495ae0915c86133fca2f7b
Ben Asher <benasher44@gmail.com>
2020-11-23 11:02:25 -0800

    Prepare for 0.2.3 release (#109)
    Upgrade gradle, kotlin, and ktlint + prep for next release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.konan.target.HostManager
 
 plugins {
-    kotlin("multiplatform") version "1.4.10"
+    kotlin("multiplatform") version "1.4.20"
     id("org.jetbrains.dokka") version "0.9.18"
     id("maven-publish")
     id("signing")
@@ -142,7 +142,7 @@ kotlin {
 val ktlintConfig by configurations.creating
 
 dependencies {
-    ktlintConfig("com.pinterest:ktlint:0.37.2")
+    ktlintConfig("com.pinterest:ktlint:0.39.0")
 }
 
 val ktlint by tasks.registering(JavaExec::class) {
