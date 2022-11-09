5a2424b5c815b0d31403d1de1a6ae96f48deab02
Ben Asher <benasher44@gmail.com>
2022-11-08 20:40:38 -0800

    Update platform.kt

--- src/nativeMain/kotlin/platform.kt
+++ src/nativeMain/kotlin/platform.kt
@@ -3,5 +3,5 @@ package com.benasher44.uuid
 import kotlin.native.concurrent.freeze
 import kotlin.native.FreezingIsDeprecated
 
-@OptIn(FreezingIsDeprecated::class).
+@OptIn(FreezingIsDeprecated::class)
 internal actual fun <T> T.freeze() = this.freeze()
