5c37fcc385e3e1d4f6f23c4c5a21bda68f5a96e3
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-11-10 17:25:11 +0100

    leave apiVersion at 1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ kotlin {
             compilations.all {
                 kotlinOptions {
                     jvmTarget = "1.8"
-                    apiVersion = "1.9"
+                    apiVersion = "1.7"
                     languageVersion = "1.9"
                 }
             }
